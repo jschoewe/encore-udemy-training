c9769537f908c45f565ded515f6b7d45c537282d
John Schoewe <john.schoewe@encore.tech>
2019-07-15 09:32:32 -0400

    Changed the service to restart if the conf file changes

--- cockpit/manifests/init.pp
+++ cockpit/manifests/init.pp
@@ -4,18 +4,18 @@ class cockpit {
     ensure => installed,
   }
 
-  service {'cockpit':
-    ensure  => running,
-    require => Package['cockpit'],
-  }
-
   file { '/etc/cockpit/cockpit.conf':
     ensure  => file,
     content => template('cockpit/cockpit.conf.erb'),
     group   => root,
     owner   => root,
     mode    => '0644',
-    require => Service['cockpit'],
+    require => Package['cockpit'],
+  }
+
+  service {'cockpit':
+    ensure    => running,
+    subscribe => File['/etc/cockpit/cockpit.conf'],
   }
 
 }
